1b0428b225956b4ab2b70c6ee187f40bc1001dc5
りんご <hanacoi7243@gmail.com>
2025-12-16 23:18:20 +0900

    Add files via upload

---
+++ AlgorithmSample/AlgorithmSample/AlgorithmSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlgorithmSampleApp.swift
+//  AlgorithmSample
+//
+//  Created by 2230220 on 2025/12/16.
+//
+
+import SwiftUI
+
+@main
+struct AlgorithmSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlgorithmSample/AlgorithmSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AlgorithmSample
+//
+//  Created by 2230220 on 2025/12/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
